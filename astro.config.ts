4ebc0b197905b8e17af5ca2c67b6ebc7275f0bfd
nuro <4991309+NuroDev@users.noreply.github.com>
2022-08-16 05:27:15 +0000

    Added Tailwind CSS

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,9 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 /**
  * @see https://astro.build/config
  */
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()]
+});
